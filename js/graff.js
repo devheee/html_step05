5493047b698c6923fd951d1c5db2ef9a93258edb
SOHEE <118507151+devheee@users.noreply.github.com>
2022-11-28 15:25:27 +0900

    Add files via upload

---
+++ js/graff.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('.topBanner i').on('click', function () {
+        $('.topBanner').addClass('on')
+    })
+
+
+
+
+
+
+
+
+
+
+})
---
+++ js/main.js
@@ -0,0 +1,32 @@
+// document.querySelector('h1').style.display = 'none';
+
+$(function () {
+    $('button').on('click', function () {
+        $('h1').toggleClass('on')
+    });
+    $('.mainSlide').slick()
+
+    // $('.tabMenu .tit>li').on('click', function () {
+    //     var num = $(this).index();
+    //     console.log(num);
+    //     $('.tabMenu .tit>li').removeClass('on')
+    //     $(this).addClass('on');
+
+    //     $('.tabMenu .con>li').removeClass('on')
+    //     $('.tabMenu .con>li').eq(num).addClass('on');
+    // })
+
+})
+
+// $('선택자').on('이벤트', 걸었을때 () {
+//    $실행 할 일}
+
+// window.addEventListener('DOMContentLoaded', function () {
+//     document.querySelector('button').addEventListener('click', function ({
+//         document.querySelector('h1').classList.toggle('on')
+//     })
+// })
+
+// < button onClick={() => h1.classList.toggle()}>
+
+// </ >
